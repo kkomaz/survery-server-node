ce1135bf421f48106f1c9e4f2eb11e82746d1ee5
Alex Lee <alex@uniteus.com>
2018-04-08 17:36:51 -0400

    Cookie Session

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 const app = express(); // single app.
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000, // How long can cookie exist before expiration (ms)
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
