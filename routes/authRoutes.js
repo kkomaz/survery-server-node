2c661ba484bb9af2f6b77aa2fc812e2e4934b13f
Alex Lee <alex@uniteus.com>
2018-04-08 17:55:58 -0400

    Logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,6 +9,11 @@ module.exports = (app) => {
   // Returned callback route post user approval
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // kills the cookie via passport
+    res.send(req.user);
+  });
+
   // Test to make sure o-auth and login flow works.
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
