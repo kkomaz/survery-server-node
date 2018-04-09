53c3e22263a5713132b252992ab18bd093c6438e
Alex Lee <alex@uniteus.com>
2018-04-09 16:18:35 -0400

    Set Proxy to true

--- services/passport.js
+++ services/passport.js
@@ -26,7 +26,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback' // route user will be sent to after granting permission
+  callbackURL: '/auth/google/callback', // route user will be sent to after granting permission
+  proxy: true
 }, (accessToken, refreshToken, profile, done) => {
   User.findOne({ googleId: profile.id })
     .then((existingUser) => {
