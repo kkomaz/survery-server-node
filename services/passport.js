ed6af02fabb2c40ecc99e40750a479cd229c9c0c
Alex Lee <alex@uniteus.com>
2018-04-10 12:41:01 -0400

    Refactor to async await

--- services/passport.js
+++ services/passport.js
@@ -9,34 +9,36 @@ const User = mongoose.model('users');
 // Serialize the user
 // user argument is coming from .save returned promise
 passport.serializeUser((user, done) => {
-  done(null, user.id)
+  done(null, user.id);
 });
 
 // Deserialize user
 // id is coming from the done of serializeUser
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((user) => {
-      done(null, user);
-    });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 // new instance of GoogleStrategy
 // passport.use (above)
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback', // route user will be sent to after granting permission
-  proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback', // route user will be sent to after granting permission
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
       if (existingUser) {
-        done(null, existingUser); // (error, existingUser)
-      } else {
-        new User({ googleId: profile.id }) // Make a new record instance - async
-          .save() // save
-          .then((user) => done(null, user));
+        return done(null, existingUser); // (error, existingUser)
       }
-    });
-}));
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
