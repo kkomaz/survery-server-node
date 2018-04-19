51da28f93512a12c45fbcee8e933ab2808fb6f83
Alex Lee <alex@uniteus.com>
2018-04-19 13:52:31 -0400

    Auth Reducer

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
 // new instance of redux-store
 // 1st arg - dummy reducer
 // 2nd arg - initial State of your application.  More Relevant for SSR
 // 3rd arg - applyMiddleware
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
