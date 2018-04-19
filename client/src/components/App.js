4be6796bcf90d28a05df6292ef0e9eecba92e546
Alex Lee <alex@uniteus.com>
2018-04-19 14:14:35 -0400

    React-Router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveryNew = () => <h2>SurveryNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
     <div>
-      Hi There!
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveryNew} />
+        </div>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
 export default App;
