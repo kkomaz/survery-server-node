15fb35b4a320c08eb6b20374cba8de711a085e8f
Alex Lee <alex@uniteus.com>
2018-04-30 23:07:36 -0400

    Add Billing Routes

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 const app = express(); // single app.
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 1000, // How long can cookie exist before expiration (ms)
@@ -20,6 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 mongoose.connect(keys.mongoURI);
 
