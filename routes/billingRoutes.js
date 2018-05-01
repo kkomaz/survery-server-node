f3f0243c2499e73204fbe3c7f10873a959d57cf9
Alex Lee <alex@uniteus.com>
2018-04-30 23:27:58 -0400

    Validate User in billing

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,10 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = (app) => {
   app.post('/api/stripe', async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: 'You must login!' });
+    }
+
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
@@ -10,6 +14,9 @@ module.exports = (app) => {
       source: req.body.id,
     });
 
-    console.log(charge);
+    req.user.credits += 5;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 }
