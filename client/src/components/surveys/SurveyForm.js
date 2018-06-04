e74e31fb84abcc5de994117e24a6a7672e5f2dfd
Alex Lee <alex@uniteus.com>
2018-06-04 09:07:22 -0400

    Custom Component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form'; // Nearly identical to connect helper.  Allows component to talk to redux store
+import { reduxForm, Field } from 'redux-form'; // Nearly identical to connect helper.  Allows component to talk to redux store
+import SurveyField from './SurveyField';
 
 class SurveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    )
+  }
   render() {
     return (
-      <div>SurveyForm</div>
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Send</button>
+        </form>
+      </div>
     );
   }
 }
